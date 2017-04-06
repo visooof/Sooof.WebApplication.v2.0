9a54bca3f9ee6bc9ef04ca65a48fd47d88e161a2
visooof <vahram.iritsyan@sooofinc.com>
2017-04-06 11:56:51 -0400

    WebApi project and sample angular call (not working yet)

---
+++ src/Sooof.WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sooof.WebApi.ViewModels;
+using Newtonsoft.Json;
+
+namespace Sooof.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NotFound(new { Error = "not found" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return new JsonResult(GetSampleItems()
+            .Where(i => i.Id == id)
+            .FirstOrDefault(),
+            DefaultJsonSettings);
+        }
+
+        [HttpGet("GetLatest")]
+        public IActionResult GetLatest()
+        {
+            return GetLatest(DefaultNumberOfItems);
+        }
+
+        [HttpGet("GetLatest/{n}")]
+        public IActionResult GetLatest(int n)
+        {
+            if (n > MaxNumberOfItems) n = MaxNumberOfItems;
+            var items = GetSampleItems().OrderByDescending(i =>
+            i.CreatedDate).Take(n);
+            return new JsonResult(items, DefaultJsonSettings);
+        }
+
+        [HttpGet("GetMostViewed")]
+        public IActionResult GetMostViewed()
+        {
+            return GetMostViewed(DefaultNumberOfItems);
+        }
+
+        [HttpGet("GetMostViewed/{n}")]
+        public IActionResult GetMostViewed(int n)
+        {
+            if (n > MaxNumberOfItems) n = MaxNumberOfItems;
+            var items = GetSampleItems().OrderByDescending(i =>
+            i.ViewCount).Take(n);
+            return new JsonResult(items, DefaultJsonSettings);
+        }
+
+        [HttpGet("GetRandom/{n}")]
+        public IActionResult GetRandom(int n)
+        {
+            var items = GetSampleItems().OrderBy(i =>
+            Guid.NewGuid()).Take(n);
+            return new JsonResult(items, DefaultJsonSettings);
+        }
+
+        private List<ItemViewModel> GetSampleItems(int num = 999)
+        {
+            List<ItemViewModel> lst = new List<ItemViewModel>();
+            DateTime date = new DateTime(2015, 12, 31).AddDays(-num);
+            for (int id = 1; id <= num; id++)
+            {
+                lst.Add(new ItemViewModel()
+                {
+                    Id = id,
+                    Title = String.Format("Item {0} Title", id),
+                    Description = String.Format("This is a sample description for item {0}: Lorem ipsum dolor sit amet.", id),
+                    CreatedDate = date.AddDays(id),
+                    LastModifiedDate = date.AddDays(id),
+                    ViewCount = num - id
+                });
+            }
+            return lst;
+        }
+
+        private JsonSerializerSettings DefaultJsonSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                };
+            }
+        }
+
+        private int DefaultNumberOfItems
+        {
+            get
+            {
+                return 5;
+            }
+        }
+        private int MaxNumberOfItems
+        {
+            get
+            {
+                return 100;
+            }
+        }
+
+    }
+}
+
--- src/sooof-app/src/app/app.module.ts
+++ src/sooof-app/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ItemListComponent } from "./item-list.component";
+import { ItemService } from "./item.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
